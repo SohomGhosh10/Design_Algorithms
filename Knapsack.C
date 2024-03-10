a49cae22013a56ac18d97ddcfb212b24c7211f98
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-03-10 10:31:54 +0530

    Update Knapsack.C

--- Knapsack.C
+++ Knapsack.C
@@ -7,6 +7,8 @@ int main(){
     int profits[n];
     int weights[n];
 
+    // Taking inputs
+
     for(int i = 0; i < n; i++){
         scanf("%d",&profits[i]);
     }
@@ -15,9 +17,13 @@ int main(){
         scanf("%d",&weights[i]);
     }
 
+    // Profit / weight ratio
+
     for(int i = 0; i < n; i++){
         result[i] = ((float)profits[i] / (float)weights[i]);
     }
+
+    // Sorting the datas in ascending order
 
     for(int i = 0; i < n - 1; i++){
         for(int j = 0; j < n - i - 1; j++){
@@ -38,11 +44,15 @@ int main(){
         }
     }
 
+    // Display
+
     for(int i = 0; i < n ; i++){
         printf("%d\n",result[i]);
         printf("%d\n",profits[i]);
         printf("%d\n",weights[i]);
     }
+
+    // Enter knapsack capacity
 
     int capacity;
     printf("Enter knapsack capacity\n");
@@ -52,6 +62,7 @@ int main(){
     float maxProfit = 0.0;
     float remainingCapacity;
 
+    // Calculating maximum profit
     for(int i = 0; i < n; i++){
         if(currentWeight + weights[i] <= capacity){
             currentWeight = currentWeight + weights[i];
